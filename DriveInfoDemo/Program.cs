cc41310b05fe27b5f43da72502a94cdd3c4ead74
umitliguler <info.umitguler@gmail.com>
2018-06-17 11:51:40 +0300

    first

---
+++ DriveInfoDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DriveInfoDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            foreach (var driveInfo in DriveInfo.GetDrives())
+            {
+                Console.WriteLine("---------------------------------");
+
+                Console.WriteLine("DriveType : " + driveInfo.DriveType);
+
+                if (driveInfo.IsReady)
+                {
+                    Console.WriteLine("Name : " + driveInfo.Name);
+                    Console.WriteLine("RootDirectory : " + driveInfo.RootDirectory);
+                    Console.WriteLine("TotalFreeSpace : " + (driveInfo.TotalFreeSpace/1024/1024/1024));
+                }
+
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
